6f960f3e8641f59a457f07863b0834e9e50c2746
MarcKVR <marcelino_8412@hotmail.com>
2024-11-25 17:42:52 -0600

    First commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Welcome to te mortgage app!")
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
