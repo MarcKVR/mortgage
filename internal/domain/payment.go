3ac47d92d4f7b221b317f0a22f5a1099a83f159f
MarcKVR <marcelino_8412@hotmail.com>
2024-11-25 20:28:36 -0600

    Add new connection

---
+++ internal/domain/payment.go
@@ -0,0 +1,31 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Payment struct {
+	ID              string         `json:"id" gorm:"type:char(36);not null;primaryKey;unique_index"`
+	PaymentNumber   int32          `json:"payment_number" gorm:"type:int;not null"`
+	DatePayment     time.Time      `json:"date_payment"`
+	Rate            int32          `json:"rate" gorm:"type:int;not null"`
+	MonthlyPayment  float64        `json:"monthly_payment" gorm:"type:decimal(10,2);not null"`
+	DamageInsurance float64        `json:"damage_insurance" gorm:"type:decimal(10,2);not null"`
+	LifeInsurance   float64        `json:"life_insurance" gorm:"type:decimal(10,2);not null"`
+	Interests       float64        `json:"interests" gorm:"type:decimal(10,2);not null"`
+	Capital         float64        `json:"capital" gorm:"type:decimal(10,2);not null"`
+	CreatedAt       *time.Time     `json:"-"`
+	UpdatedAt       *time.Time     `json:"-"`
+	Deleted         gorm.DeletedAt `json:"-"`
+}
+
+func (c *Payment) BeforeCreate(tx *gorm.DB) (err error) {
+	if c.ID == "" {
+		c.ID = uuid.New().String()
+	}
+
+	return
+}
--- main.go
+++ main.go
@@ -1,21 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		AppName: "Mortgage control",
+	})
 
+	// Ruta raíz de la aplicación
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Welcome to te mortgage app!")
 	})
 
-	app.Use(func(c *fiber.Ctx) error {
-		return c.Status(404).SendString("Sorry can't find that!")
-	})
+	// // Agrupador de rutas
+	// api := app.Group("/api")
+	// v1 := api.Group("/v1")
+	// v1.Get("/users", func(c *fiber.Ctx) error {
+	// 	return c.SendString("Welcome to users API!")
+	// })
+
+	// // Rutas con prefijo
+	// app.Route("/admin", func(api2 fiber.Router) {
+	// 	api2.Get("/forms", func(c *fiber.Ctx) error {
+	// 		return c.SendString("Welcome to admin FORMS Resource!")
+	// 	})
+
+	// 	api2.Get("/actions", func(c *fiber.Ctx) error {
+	// 		return c.SendString("Welcome to admin ACTIONS Resource!")
+	// 	})
+	// })
+
+	// // Ruta por defecto de No encontrado
+	// app.Use(func(c *fiber.Ctx) error {
+	// 	return c.Status(404).SendString("Sorry can't find that!")
+	// })
+
+	dsn := "host=localhost user=mortgage_user password=mortgage_pass dbname=mortgage_db port=5439 sslmode=disable TimeZone=America/Mexico_City"
+	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Connection to the database was successful!")
+	}
 
 	log.Fatal(app.Listen(":3000"))
 }
